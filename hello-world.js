f72aa3e725095de634fd40a1835aa34377acc8bf
Mark Jesper Pillerva <83439062+Shironekomaru69@users.noreply.github.com>
2021-06-07 09:39:50 +0800

    Add files via upload

---
+++ hello-world.js
@@ -0,0 +1,8 @@
+<<<<<<< HEAD
+const sayHello = name => console.log('Hello ' + name);
+
+=======
+const sayHello = name => console.log('Hello ' + name);
+
+>>>>>>> c6ae651f27b3fa9fae9f5dcb5ef6b3151fa61801
+sayHello('Mark');
